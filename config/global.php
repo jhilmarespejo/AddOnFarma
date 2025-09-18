c89f8a277f34f2cbce53405e116bf9f125698273
jhilmarespejo <jhilmar.e.f@gmail.com>
2025-09-18 10:59:35 -0400

    Eliminando carpeta config del seguimiento de Git

---
+++ config/global.php
@@ -0,0 +1,23 @@
+<?php 
+//Ip de la pc servidor de base de datos
+define("DB_HOST","localhost");
+
+//Nombre de la base de datos
+//define("DB_NAME", "innovasa_AddOnInnova");
+define("DB_NAME", "farma");
+
+//Usuario de la base de datos
+//define("DB_USERNAME", "innovasa_AddOnInnova");
+define("DB_USERNAME", "root");
+
+//Contrase�0�9a del usuario de la base de datos
+//define("DB_PASSWORD", "Pr0y3ct0C1d4!");
+//define("DB_PASSWORD", "Innova#54321$");
+define("DB_PASSWORD", "");
+
+//definimos la codificaci��n de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","Proyecto Admision & Cobranzas");
+?>
